db2f3cc095046b6521ba5a156296e6451e3220b1
John-Mark Sackett <jmsackett91@gmail.com>
2021-08-21 14:55:37 -0400

    finished up my schema file and wrote my seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO departments (name)
+VALUES ('Sales'),
+       ('Engineering'),
+       ('Finance'),
+       ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Lead Engineer', 150000, 2),
+       ('Legal Team Lead', 250000, 4),
+       ('Acount Manager', 160000, 3),
+       ('Sales Lead', 100000, 1),
+       ('Accountant', 125000, 3),
+       ('Sales Person', 80000, 1),
+       ('Software Engineer', 120000, 2),
+       ('Lawyer', 190000, 4);
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUES ('John', 'Smith', null, 1),
+       ('Jane', 'Doe', null, 2),
+       ('Marcus', 'Aurelius', null, 3),
+       ('Jean', "D'arc", null, 4),
+       ('Alexander', 'Macedonia', 3, 5),
+       ('Marvin', 'Martian', 4, 6),
+       ('Bugs', 'Bunny', 1, 7),
+       ('Ally', 'McBeal', 2, 8);
