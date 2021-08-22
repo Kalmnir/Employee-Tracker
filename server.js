d7b3ed5086014c53a04f0ce9cf8c794c570ffd3c
John-Mark Sackett <jmsackett91@gmail.com>
2021-08-22 13:22:54 -0400

    file restructuring since I cant figure out how to properly link the 3 different files together

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const connection = require('./db/connection');
+const cTable = require(console.table);
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'rootr00t!',
+    database: 'employee_db'
+});
 
 connection.connect((err) => {
     if (err) {
@@ -8,10 +16,10 @@ connection.connect((err) => {
         throw err;
     }
     console.log(`Connected to server with ID ${connection.threadId}`)
-    startprompt();
+    startPrompt();
 });
 
-function startprompt() {
+function startPrompt() {
     inquirer.prompt([
         {
             type: 'list',
@@ -30,15 +38,15 @@ function startprompt() {
     ]).then(function (val) {
         switch (val.choice) {
             case 'View all employees':
-
+                viewAllEmployees()
                 break;
 
             case 'View all employees by role':
-
+                viewAllRoles
                 break;
 
             case 'View all employees by manager':
-
+                viewAllDepartments
                 break
 
             case 'Update employee':
@@ -58,4 +66,29 @@ function startprompt() {
                 break
         }
     })
+}
+
+function viewAllEmployees() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+        function (err, res) {
+            if (err) throw err
+            console.table(res)
+            startPrompt()
+        })
+}
+function viewAllRoles() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id",
+        function (err, res) {
+            if (err) throw err
+            console.table(res)
+            startPrompt
+        })
+}
+function viewAllDepartments() {
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;",
+        function (err, res) {
+            if (err) throw err
+            console.table(res)
+            startPrompt
+        })
 }
